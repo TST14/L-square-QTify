ccbffd9bbdef70a3d33eb9a37865937119552029
Tanmay Singh Thakur <thakurtanmaysingh@gmail.com>
2024-02-24 19:05:10 +0530

    placeholder search

--- qtify/src/components/SearchBar/SearchBar.jsx
+++ qtify/src/components/SearchBar/SearchBar.jsx
@@ -2,14 +2,10 @@ import React from "react";
 import styles from "./SearchBar.module.css";
 import { ReactComponent as SearchIcon } from "../../assets/search-icon.svg";
 
-function SearchBar() {
+function SearchBar({ search }) {
   return (
     <div className={styles.container}>
-      <input
-        name="search"
-        className={styles.search}
-        placeholder="Enter an Album"
-      />
+      <input name="search" className={styles.search} placeholder={search} />
       <button className={styles.searchButton}>
         <SearchIcon />
       </button>
